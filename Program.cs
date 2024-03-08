125e33d3efe5e6f36026ad046158456398b23d0b
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2024-03-07 20:46:23 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using MyClassLibrary;
+
+
+namespace Class_Libraries_JuanHernandez
+{
+
+    // Juan Hernandez
+    // Class Libraries
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DiceRoll.RollTheDie(20);
+            Console.WriteLine(DiceRoll.RollTheDie(20));
+        }
+    }
+}
